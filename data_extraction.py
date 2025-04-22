5f36589761029307eaa836c1759af72378e33c81
Krishna Mohan <krishanmohank974@gmail.com>
2025-04-23 02:19:34 +0530

    fixed directory in data extraction

--- data_extraction.py
+++ data_extraction.py
@@ -1,5 +1,5 @@
 from sec_edgar_downloader import Downloader
 
-dl = Downloader("sec-edgar-data", "krishanmohank974@gmail.com", "sec-edgar-data")
+dl = Downloader("sec-edgar-data", "krishanmohank974@gmail.com")
 
 dl.get("10-K", "AAPL")
